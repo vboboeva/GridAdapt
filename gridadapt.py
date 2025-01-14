6c04d5f7d421cfdaba37b92b1b710c62694d074b
vboboeva <vezha.boboeva@gmail.com>
2025-01-14 16:50:56 +0000

    Important commit: this version manages to converge g and theta to their target values

--- gridadapt.py
+++ gridadapt.py
@@ -16,22 +16,21 @@ from ratinabox.Agent import Agent
 from ratinabox.Neurons import PlaceCells, GridCells
 import matplotlib.pyplot as plt
 from scipy.optimize import minimize
-from tqdm import tqdm
 
 def main():
 
 	SimulationName="test"
-	# default values are reported in brackets
 	random.seed(2024)
-	N_I=200 # [200]
-	N_mEC=100 # [100]
-	dt=0.01 # [?]
-	tau_1=1 # [b1=dt/tau_1=0.01]
-	tau_2=3 # [b2=dt/tau_2=b1/3=0.01/3=0.003]
-	psi_sat=30 # [30]
-	a0=0.1*psi_sat # 0.1 is fraction of active neurons [0.1*psi_sat]
-	s0=0.3 # [0.3]
-	lr=0.001 # [epsilon in paper = 0.001]
+	N_I=200
+	N_mEC=100
+	dt=0.001
+	tau_1=10
+	tau_2=30
+	psi_sat=30
+	a0=0.1*psi_sat # 0.1 is fraction of active neurons
+	s0=0.3 
+	epsilon=0.001
+	lr=0.001
 
 	ratinabox.autosave_plots = True
 	ratinabox.figure_directory = "figs"
@@ -45,15 +44,15 @@ def main():
 		os.makedirs(d, exist_ok=True)
 
 	# 1 Initialise environment.
-	Env = Environment(params={"aspect": 1, "scale": 20})
+	Env = Environment(params={"aspect": 1, "scale": 1})
 
 	# 3 Add Agent.
-	Ag = Agent(Env, params={"dt": dt})
-	Agent.speed_mean = 3 #m/s
-	Ag.pos = np.array([10, 10])
-	n_steps = int(1e7)
-	tol_a=0.1
-	tol_s=0.1
+	Ag = Agent(Env)
+	Agent.speed_mean = 0.5 #m/s
+	Ag.pos = np.array([0.5, 0.5])
+	n_steps = 100000 #int(2000/Ag.dt)
+	tol_a=0.01
+	tol_s=0.01
 
 	# 4 Add place cells.
 	PCs = PlaceCells(
@@ -61,34 +60,27 @@ def main():
 		params={
 			"n": N_I,
 			"description": "gaussian",
-			"widths": 1,
-			"max_fr": 30,
+			"widths": 0.01,
+			"max_fr": 1,
 			"min_fr": 0.1,
 			"color": "C1",
 		},
 	)
-
-	r=np.zeros(N_I)
-	r_tempmean=np.zeros(N_I)
-	
 	h=np.zeros(N_mEC)
+	r=np.zeros(N_I)
 	r_act=np.zeros(N_mEC)
 	r_inact=np.zeros(N_mEC)
 	psi=np.zeros(N_mEC)
 	psi_tempmean=np.zeros(N_mEC)
+	r_tempmean=np.zeros(N_I)
+	J=np.random.random((N_mEC, N_I))
 
-	# J=np.zeros((N_mEC, N_I))
-	# J=np.random.random((N_mEC, N_I))
-
-	J = 0.9 + 0.1*np.random.uniform(size=(N_mEC, N_I)) # random weights (from hc to ec) initialization
-	sum_weight = np.sum(J**2, axis=1)
-	J /= np.sqrt(sum_weight)[:, None]
 
 	# create space domain to visualize firing fields
 	x_min, y_min = 0., 0. # ...
-	x_max, y_max = 20., 20. # ...
-	n_points_x = 40
-	n_points_y = 40
+	x_max, y_max = 1., 1. # ...
+	n_points_x = 30
+	n_points_y = 30
 	_dx, _dy = (x_max - x_min)/n_points_x, (y_max - y_min)/n_points_y
 	xs, ys = np.meshgrid(np.linspace(x_min, x_max, n_points_x),
 						 np.linspace(y_min, y_max, n_points_y)
@@ -100,13 +92,12 @@ def main():
 	_trajectory = np.zeros((2,n_steps))
 	neuron_ids = np.arange(50) # np.random.choice(N_mEC, 4) 
 	place_ids = np.arange(50) # np.random.choice(N_I, 3) 
-	snapshots = np.arange(0, n_steps, 1000)
+	snapshots = np.arange(0, n_steps, 200)
 
-	theta=0.0
-	g=1
+	theta=0.1
+	g=0.1
 
 	for step in range(n_steps):
-		# print(step)
 
 		Ag.update()
 		PCs.update()
@@ -115,49 +106,80 @@ def main():
 		# fig, ax = PCs.plot_rate_timeseries()
 
 		r = np.ravel(PCs.history['firingrate'][step])
-		for neuron_idx in range(N_mEC):
-			# print('r', r)
-			# print('\n')
-			h[neuron_idx] = np.dot(J[neuron_idx], r)/N_I  
-			# print('h', h)
-			# print('\n')
-			r_act[neuron_idx] += dt*(h[neuron_idx]-r_inact[neuron_idx]-r_act[neuron_idx])/tau_1
-			# print('r_act', r_act)
-			# print('\n')
-			r_inact[neuron_idx] += dt*(h[neuron_idx]-r_inact[neuron_idx])/tau_2
-			# print('r_inact', r_inact)
-			# print('\n')
+		# print('r', r)
+		# print('\n')
+		h = np.dot(J, r)#/N_I # normalization of the weights breaks the optimization of theta and g...
 
-			psi[neuron_idx] = transfer(r_act[neuron_idx], theta, g, psi_sat)
-		# s = np.mean(psi)**2/(np.mean(psi**2)+0.00001)
-		# print(psi)
-		# print('before sparsify', s)
-		psi = Sparsify(psi, s0)
-		# print(psi)
-		# s = np.mean(psi)**2/(np.mean(psi**2)+0.00001)
-		# print('after sparsify', s)
-		psi = psi/(np.mean(psi)+0.00001)
-		s = np.mean(psi)**2/(np.mean(psi**2)+0.00001)
+		# print('h', h)
+		# print('\n')
+		r_act += dt*(h-r_inact-r_act)/tau_1
+		# print('r_act', r_act)
+		# print('\n')
+		r_inact += dt*(h-r_inact)/tau_2
+		# print('r_inact', r_inact)
+		# print('\n')
+
+		psi = transfer(r_act, theta, g, psi_sat)
 		a = np.mean(psi)
+		s = np.sum(psi)**2/(N_mEC*np.sum(psi**2)+0.00001)
 
-		print('a', a, a0)
-		print('s', s, s0)
+		delta_a = 1000
+		delta_s = 1000
+
+		if (delta_a > tol_a) or (delta_s > tol_s):
+			# 1. find threshold to match sparsity
+
+			def sparsity (theta):
+				psi = transfer(r_act, theta, g, psi_sat)
+				s = np.sum(psi)**2/(N_mEC*np.sum(psi**2)+0.00001)
+				return s
+
+			def distance(x):
+				_theta = x
+				s = sparsity(_theta)
+				return (s-s0)**2
+
+			opt = minimize(distance, np.array([theta]))
+			theta = opt['x'][0]
+
+			# 2. find gain to match activity
+			def activity (theta, g):
+				psi = transfer(r_act, theta, g, psi_sat)
+				a = np.mean(psi)
+				return a
+
+			def distance(x):
+				_theta, _g = x
+				a = activity(_theta,_g)
+				return (a-a0)**2
+
+			opt = minimize(distance, np.array([theta, g]))
+			theta, g = opt['x']
+
+			# 3. compute errors
+			delta_a = np.abs(a - a0)/a0
+			delta_s = np.abs(s - s0)/s0
+
+		print(step)
+		print(f'a = {a:.5f},   s = {s:.5f},   delta_a = {delta_a:.2f},   delta_s = {delta_s:.2f},  theta = {theta:.2f},   g = {g:.2f}')
+
+		psi = transfer(r_act, theta, g, psi_sat)
 
 		psi_tempmean = (psi + step*psi_tempmean)/(step+1)
 		r_tempmean = (r + step*r_tempmean)/(step+1) 
 
-		# update weights
-		J += lr*( psi[:,None]*r[None,:] - (psi_tempmean[:,None]*r_tempmean[None,:]) ) 
-
+		J += lr*(np.outer(psi,r) - np.outer(psi_tempmean, r_tempmean))
+		
+		# this part of the code breaks the optimization of theta and g...
 		# set negative values to zero
-		J[np.where(J<0)] = 0.
-		# exit()
-
+		# J[np.where(J<0)] = 0.
+		# print('before', J)
 		# for each unit in mEC, normalize all ingoing weights onto it to the sum of all of them 
-		sum_weight = np.sum(J**2, axis=1)
-		J /= np.sqrt(sum_weight)[:,None]
+		# sum_weight = np.sum(J**2, axis=1)
+		# print('sum_weight', sum_weight)
+		# J /= np.sqrt(sum_weight)[:, None]
+		# print('after', J)
 
-		# print(J)
 
 		# build the firing field for visualization
 		xt, yt = Ag.pos
@@ -167,7 +189,6 @@ def main():
 		_firing_fields += psi[:, None, None] * _kernel_map[None, :, :] / n_steps
 
 		if step in snapshots:
-			print(step)
 			fig1, axs1 = plt.subplots(5, 10, figsize=(12, 6))
 			plt.tight_layout()
 			fig2, axs2 = plt.subplots(5, 10, figsize=(12, 6))
@@ -202,93 +223,24 @@ def main():
 # FUNCTIONS
 
 def _saturating (x):
-	_z = 2.*np.arctan(x)/np.pi
-	# _z[np.where(x > 50)] = 1.
+	_z = 2./np.pi*np.arctan(x)
+	_z[np.where(x > 50)] = 1.
 	return _z
 
 def transfer(h, theta, g, psi_sat):
-	_psi = Heaviside(h-theta)*psi_sat*_saturating(g*(h-theta))
+	_psi = psi_sat*_saturating(g*(h-theta))*Heaviside(h-theta)
 	return _psi
 
 def Heaviside(h):
-	# _h = h.copy()
-	# _h[np.where(h<0)] = 0.
-	# return _h
-	return h if h>0. else 0.
+	# _h = np.array([_x if _x>=0 else 0 for _x in x])
+	_h = h.copy()
+	_h[np.where(h<0)] = 0.
+	return _h
 
-def _kernel (x, y, sigma=.5):
+def _kernel (x, y, sigma=.1):
 	_z = 0.5 * (x**2 + y**2) / (2 * sigma**2)
 	_norm = 1.
 	return np.exp(-_z)/_norm
 
-def Sparsify(field, s0):
-        vout=field
-        # print('vout', vout)
-        th=np.percentile(vout, (1.0-s0)*100)
-        # print('th', th)
-        vout = [0 if x < th else x-th for x in vout]
-        # print('vout', vout)
-        # exit()
-        return np.array(vout)
-
-# def Sparsify(V, theta):
-# 	vout=V
-# 	th=np.percentile(V,(1.0-f)*100)
-# 	for i in range(len(V)):
-# 		if vout[i]<th:
-# 			vout[i]=0
-# 		else:
-# 			vout[i]=vout[i]-th
-# 	return vout
-
 if __name__ == "__main__":
 	main()
-
-
-
-		# delta_a = 1000
-		# delta_s = 1000
-
-		# if (delta_a > tol_a) or (delta_s > tol_s):
-
-		# 	# 1. find threshold to match sparsity
-
-		# 	def sparsity (theta):
-		# 		psi = transfer(r_act, theta, g, psi_sat)
-		# 		s = np.sum(psi)**2/(N_mEC*np.sum(psi**2)+0.00001)
-		# 		return s
-
-		# 	def distance(x):
-		# 		_theta = x
-		# 		s = sparsity(_theta)
-		# 		return (s-s0)**2
-
-		# 	opt = minimize(distance, np.array([theta]))
-		# 	theta = opt['x'][0]
-
-		# 	# 2. find gain to match activity
-		# 	def activity (theta, g):
-		# 		psi = transfer(r_act, theta, g, psi_sat)
-		# 		a = np.mean(psi)
-		# 		return a
-
-		# 	def distance(x):
-		# 		_theta, _g = x
-		# 		a = activity(_theta,_g)
-		# 		return (a-a0)**2
-
-		# 	opt = minimize(distance, np.array([theta, g]))
-		# 	theta, g = opt['x']
-
-		# 	# 3. compute relative errors
-		# 	delta_a = np.abs(a - a0)/a0
-		# 	delta_s = np.abs(s - s0)/s0
-
-		# 	# print('theta', theta)
-		# 	# print('g', g)
-
-		# 	psi = transfer(r_act, theta, g, psi_sat)
-
-		# 	psi_tempmean += psi 
-		# 	r_tempmean += r 
-################################################
